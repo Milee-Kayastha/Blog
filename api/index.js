dfd5c6fc8b00ffb55cda64efb625a250cff44c72
Milee_Kayastha <mkayastha@hamropatro.com>
2024-02-21 09:58:23 +0545

    feat: connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -4,12 +4,23 @@ import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
+const MONGO_URL =
+  "mongodb+srv://milee:bangtan7@milee.zy2zgir.mongodb.net/Node-API?retryWrites=true&w=majority";
 
 app.use(cors());
 app.use(express.json());
 
 app.post("/register", (req, res) => {
+  const { username, password } = req.body;
   res.json("ok");
 });
 
-app.listen(PORT);
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(PORT, () => console.log(`Listening on port ${port}...`));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
